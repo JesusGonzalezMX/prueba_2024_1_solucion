0630fed46b8d1e23d1278c80f01c35aaf4d5aa9f
Yizwz <contacto@jesusgonzalez.mx>
2024-02-24 03:09:11 +0000

    Update with matplotlib

--- scripts/visualizacion.py
+++ scripts/visualizacion.py
@@ -1,11 +1,37 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+from matplotlib.backends.backend_agg import RendererAgg
 
-# CArgamos la informacion
-df = pd.read_csv('data/Procesamiento_salidas/razones_long.csv')
+#Loading the data
+@st.cache_resource
+def get_data_deputies():
+     return pd.read_csv('data/Procesamiento_salidas/razones_long.csv')
 
+matplotlib.use("agg")
+_lock = RendererAgg.lock
+
+#configuration of the page
+st.set_page_config(layout="wide")
+#load dataframes
+df = get_data_deputies()
 #separamos la columna del año para un mejor manejo de la informacion
 df[['variable','anio']] = df['variable'].str.rsplit("_",n=1, expand=True)
 
-# Now you can work with the DataFrame 'df'
-print(df)
+
+st.title("Visualización y análisis de la información")
+#st.subheader("Subtitulo")
+#st.text('Esta')
+st.markdown("""
+Esta app ayuda a visualizar y los cambios registrados en el consumo 
+de combustible por cada tipo de transporte de 2008 a 2019. 
+Particularmente analiza los cambios en las proporciones de consumo 
+de gasolina y dísel.'
+""")
+
+st.markdown("""
+Para este fin se usara la salida del script de procesamiento, 
+en especifico el archivo en formato long "/data/Procesamiento_salidas/razones_long.csv"
+""")
+
+st.write(df)
