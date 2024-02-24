c8cd09c3e638f2cdc380d3e62e18156bd6697706
Yizwz <contacto@jesusgonzalez.mx>
2024-02-24 05:45:19 +0000

    sync final 00

--- scripts/visualizacion.py
+++ scripts/visualizacion.py
@@ -9,6 +9,7 @@ import seaborn as sns
 def get_data():
      df = pd.read_csv('data/Procesamiento_salidas/razones_long.csv').fillna('0')
      df[['tipo_combustible','anio']] = df['tipo_combustible'].str.rsplit("_",n=1, expand=True)
+     df.drop(df[df['tipo_combustible'] == 'Total'].index, inplace=True)
      convert_dict = {'modo_transporte': str,
                      'tipo_combustible': str,
                      'valor': float,
@@ -46,11 +47,15 @@ st.write(df)
 
 st.header('Evolucion del consumo domestico por modo de transporte')
 groupedvalues = df.groupby(['modo_transporte','anio']).sum().reset_index()
+groupedvalues.drop(groupedvalues[groupedvalues['modo_transporte'].isin(['HIGHWAY ','NONHIGHWAY'])==True].index, inplace=True)
 fig = plt.figure(figsize=(10, 4))
 sns.barplot(data=groupedvalues, y='valor', x='anio', hue='modo_transporte')
 st.pyplot(fig)
 st.markdown("""
-En la grafica anterior podemos observar 
+En la grafica anterior podemos observar que el uso de combustibles por modo de 
+transporte ha mantenido la proporcion a lo largo de los años, sin embargo se ha reducido 
+al rededor del 50% para los vehiculos ligeros y medianos, que son los que consumen una 
+mayor cantidad de comnbustible.
 """)
 
 st.header('Evolucion del consumo domestico por tipo de combustible')
@@ -58,3 +63,20 @@ groupedvalues = df.groupby(['tipo_combustible','anio']).sum().reset_index()
 fig = plt.figure(figsize=(10, 4))
 sns.barplot(data=groupedvalues, y='valor', x='anio', hue='tipo_combustible')
 st.pyplot(fig)
+st.markdown("""
+En la grafica anterior podemos observar que el uso de combustibles segun su tipo
+ha mantenido la proporcion a lo largo de los años, salvo el consumo de gas natural,
+el cual se ha reducido muchisimo.
+""")
+
+st.header('Cambios en las proporciones de consumo de gasolina y dísel')
+groupedvalues = df.groupby(['tipo_combustible','anio']).sum().reset_index()
+groupedvalues.drop(groupedvalues[groupedvalues['tipo_combustible'].isin(['Gasoline','Diesel_fuel'])==False].index, inplace=True)
+fig = plt.figure(figsize=(10, 4))
+sns.barplot(data=groupedvalues, y='valor', x='anio', hue='tipo_combustible')
+st.pyplot(fig)
+st.markdown("""
+En la grafica anterior podemos observar que aunque de manera lenta, existe una ligeria tendencia 
+a usar mas disel y menos gasolina.
+""")
+
